9fe97c7b60cbe2d556c3d4587cc03242d7a481e4
Anthony Morris <anthony@amorrissound.com>
2019-04-13 12:32:49 -0700

    Add deployment of organization token

--- daos.js
+++ daos.js
@@ -4,8 +4,18 @@ const Web3 = require('web3');
 const mintOrganizationToken = async (db, { name, symbol }) => {
   const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
   
-  const { abi } = JSON.parse(fs.readFileSync('./build/contracts/OrganizationToken.json', 'utf8'));
+  const { abi, bytecode } = JSON.parse(fs.readFileSync('./build/contracts/OrganizationToken.json', 'utf8'));
   let organizationToken = web3.eth.Contract(abi);
+
+  // Mints tokens to Legion which need to be transfered later
+  const { address: tokenAddress } = await organizationToken.deploy({
+    data: bytecode,
+    arguments: [name, symbol, 18, '0xAB0b6e4eBA3985b31E826202FE0Dd9688620427e'],
+  })
+    .send({ from: '0xAB0b6e4eBA3985b31E826202FE0Dd9688620427e', gas: 4712388, gasPrice: 100000000000 })
+    .catch(err => console.log('Shit, something went wrong deploying the org token.', err));
+
+  db.collection('daos').updateOne({ name }, { $set: { tokenAddress } });
 };
 
 const createDao = async (db, { name, symbol }) => {
@@ -13,6 +23,8 @@ const createDao = async (db, { name, symbol }) => {
     name,
     symbol,
   });
+
+  mintOrganizationToken(db, { name, symbol });
 
   return;
 }
--- players.js
+++ players.js
@@ -7,13 +7,13 @@ const mintPlayerToken = async (db, { name, height, number }) => {
   const { abi } = JSON.parse(fs.readFileSync('./build/contracts/PlayerToken.json', 'utf8'));
   let playerToken = web3.eth.Contract(abi, '0x0c842539c0Fa1dD1287EE51f96724b98f09C5fa2');
 
-  playerToken.methods.createPlayerToken(name, height, number, 'http://whatever.com')
+  const tokenId = web3.utils.soliditySha3(name, height, number);
+
+  playerToken.methods.createPlayerToken(name, height, number, `http://localhost:8080/players/${tokenId}`)
     .send({ from: '0xAB0b6e4eBA3985b31E826202FE0Dd9688620427e', gas: 400000 }, (err, transaction) => {
       if (err) {
         console.log('Shit, something went wrong minting the player token.', err);
       }
-
-      const tokenId = web3.utils.soliditySha3(name, height, number);
 
       db.collection('players').updateOne({ name }, { $set: { transaction, tokenId } });
     });
