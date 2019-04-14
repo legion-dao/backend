c369399d102b5f02706db651b978ee054318f039
Anthony Morris <anthony@amorrissound.com>
2019-04-13 18:42:48 -0700

    Cleanup deployment file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
 const PlayerToken = artifacts.require('PlayerToken');
-const AssociationToken = artifacts.require('AssociationToken');
-
+const OrganizationToken = artifacts.require('OrganizationToken');
 
 module.exports = function(deployer) {
   deployer.deploy(PlayerToken);
-  deployer.deploy(AssociationToken, "knicks_token", "NICK", 18, '0x9693cEc5C937ba1Ba93e5e0ab1d83329C1EC1466');
+  deployer.deploy(OrganizationToken, "knicks_token", "NICK", 18, '0x9693cEc5C937ba1Ba93e5e0ab1d83329C1EC1466');
 };
