5be0bba7512bb8383333ba6343a861715bae5613
Anthony Morris <anthony@amorrissound.com>
2019-04-13 18:45:02 -0700

    Dynamic address during deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
+
 const PlayerToken = artifacts.require('PlayerToken');
 const OrganizationToken = artifacts.require('OrganizationToken');
 
-module.exports = function(deployer) {
+module.exports = async (deployer) => {
   deployer.deploy(PlayerToken);
-  deployer.deploy(OrganizationToken, "knicks_token", "NICK", 18, '0x9693cEc5C937ba1Ba93e5e0ab1d83329C1EC1466');
+  deployer.deploy(OrganizationToken, "knicks_token", "NICK", 18, await web3.eth.accounts[0]);
 };
